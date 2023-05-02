f97cf0744e3bcb8c49edb7758eb599284f686957
Magucho Gómez <edgar.gomez@unicafam.edu.co>
2023-05-01 20:22:06 -0500

    Se agrega la funcion y el boton copiar

--- index2.js
+++ index2.js
@@ -2,7 +2,7 @@
 function encriptar(){
     let  codifica = document.getElementById("area1").value;
     let anuncio = document.getElementById("titulo-mensaje");
-    let bloque = document.getElementById("parrafo");
+    let bloque = document.getElementById("mensajeC");
     let vistiBueno = document.getElementById("ima-inicio");
 
     let respuesta = codifica
@@ -15,11 +15,11 @@ function encriptar(){
                      if(codifica.length !=0){
                         document.getElementById("area1").value = respuesta;
                         anuncio.textContent = "Texto encriptado con exito";
-                        bloque.textContent= "";
+                        bloque.textContent= respuesta;
                         vistiBueno.src = "./imagenes/ok2.gif"
                      }else{
                         vistiBueno.src = "./imagenes/what.gif";
-                        bloque.textContent= "";
+                        bloque.textContent= " ";
                         anuncio.textContent = "No se encriptó ningun texto";
                         swal("¡OJO!","Debes ingresar un texto","warning");
                      }
@@ -30,7 +30,7 @@ function desencriptar(){
 
     let codifica = document.getElementById("area1").value;
     let anuncio = document.getElementById("titulo-mensaje");
-    let bloque = document.getElementById("parrafo");
+    let bloque = document.getElementById("mensajeC");
     let vistiBueno = document.getElementById("ima-inicio");
     
     
@@ -44,13 +44,29 @@ function desencriptar(){
                             if(codifica.length != 0){
                                 document.getElementById("area1").value = respuesta;
                                 anuncio.textContent = "Texto desencriptado con exito";
-                                bloque.textContent= "";
+                                bloque.textContent= respuesta;                               
                                 vistiBueno.src = "./imagenes/ok.gif"
                             }else{
                                 vistiBueno.src = "./imagenes/desencriptar.gif";
-                                bloque.textContent= "";
+                                bloque.textContent= " ";
                                 anuncio.textContent = "No se desencriptó ningun texto";
                                 swal("¡OJO!","Debes ingresar un texto","warning");
                             }
+}
 
+function lea(){
+     
+    let texto = document.querySelector('#mensajeC').value;
+    return texto;
+}
+
+async function co(){
+    
+    let txt = lea();
+    await navigator.clipboard.writeText(txt);
+}
+
+function copia(){
+
+    co();
 }
